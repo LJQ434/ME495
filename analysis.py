84ca411bd1180a380752ee1e1870a2cd0ded6a0a
LJQ434 <junqingleng2028@u.northwestern.edu>
2023-03-13 11:41:51 -0500

    done

---
+++ analysis.py
@@ -0,0 +1,10 @@
+#%%
+import constants as c
+from solution import SOLUTION
+import numpy
+
+best = SOLUTION(0)
+
+best.Only_simulate("GUI",0) #import the best: body0.urdf and brain0.nndy
+
+# %%
---
+++ constants.py
@@ -0,0 +1,11 @@
+
+
+numberOfGenerations= 500 # number of generations 
+populationSize = 5 #parellel population
+
+motorJointRange = 0.6
+iter= 1000 #iteration times in GUI simulation
+force = 200
+
+scaler = 0.8
+#linear random snake
